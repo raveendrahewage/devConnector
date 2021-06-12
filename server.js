d9e5bf4829c4b4769dfe111180e4ac1f8cdf3341
raveendraHewage <hewagerv@gmail.com>
2021-06-12 19:24:39 +0530

    registration and login form

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/user', user)
 app.use('/api/profile', profile)
 app.use('/api/post', post)
 
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log('Server running on port ' + port);
